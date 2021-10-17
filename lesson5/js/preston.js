3326783511c39909885429cb819abd9d034e1b91
Spearsj1 <spe21002@byui.edu>
2021-10-17 00:23:54 -0500

    Updates for the week

--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -9,3 +9,9 @@ const modifyDate = new Date();
 const options = {weekday: "long", day: "numeric" , month: "long", year: "numeric"};
 
 document.querySelector('.modify').textContent = modifyDate.toLocaleDateString('en-UK', options);
+
+const day = modifyDate.getDay();
+
+if (day != 5){
+    document.querySelector('.fri').style.display = "none";
+}
